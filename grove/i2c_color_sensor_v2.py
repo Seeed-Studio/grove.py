345027c16bc47bed411eec93670d6bbf47319a5b
Yihui Xiong <yihui.xiong@hotmail.com>
2018-06-26 18:17:01 +0800

    add Grove I2C Color Sensor V2

---
+++ grove/i2c_color_sensor_v2.py
@@ -0,0 +1,160 @@
+
+import time
+from grove.i2c import Bus
+
+
+_CMD      = 0x80
+_AUTO     = 0x20
+
+_ENABLE   = 0x00
+_ATIME    = 0x01
+_WTIME    = 0x03
+_AILT     = 0x04
+_AIHT     = 0x06
+_PERS     = 0x0C
+_CONFIG   = 0x0D
+_CONTROL  = 0x0F
+_ID       = 0x12
+_STATUS   = 0x13
+_CDATA    = 0x14
+_RDATA    = 0x16
+_GDATA    = 0x18
+_BDATA    = 0x1A
+
+_AIEN       = 0x10
+_WEN        = 0x08
+_AEN        = 0x02
+_PON        = 0x01
+
+_GAINS  = (1, 4, 16, 60)
+
+
+class I2cColorSensorV2:
+    """Driver for Grove I2C Color Sensor (TCS34725)"""
+
+    def __init__(self, bus=None, address=0x29):
+        self.address = address
+        self.bus = Bus(bus)
+
+        self.awake = False
+
+        if self.id in (0x44, 0x4D):
+            raise ValueError('Not find a Grove I2C Color Sensor')
+
+        self.set_integration_time(24)
+        self.set_gain(4)
+
+    def wakeup(self):
+        enable = self._read_byte(_ENABLE)
+        self._write_byte(_ENABLE, enable | _PON | _AEN)
+        time.sleep(0.0024)
+
+        self.awake = True
+
+    def sleep(self):
+        enable = self._read_byte(_ENABLE)
+        self._write_byte(_ENABLE, enable & ~_PON)
+
+        self.awake = False
+
+    def is_awake(self):
+        return self._read_byte(_ENABLE) & _PON
+
+    def set_wait_time(self, t):
+        pass
+
+    @property
+    def id(self):
+        return self._read_byte(_ID)
+
+    @property
+    def integration_time(self):
+        steps = 256 - self._read_byte(_ATIME)
+        return steps * 2.4
+
+    def set_integration_time(self, t):
+        """Set the integration time of the sensor"""
+        if t < 2.4:
+            t = 2.4
+        elif t > 614.4:
+            t = 614.4
+        
+        steps = int(t / 2.4)
+        self._integration_time = steps * 2.4
+        self._write_byte(_ATIME, 256 - steps)
+
+    @property
+    def gain(self):
+        """The gain control. Should be 1, 4, 16, or 60.
+        """
+        return _GAINS[self._read_byte(_CONTROL)]
+
+    def set_gain(self, gain):
+        if gain in _GAINS:
+            self._write_byte(_CONTROL, _GAINS.index(gain))
+
+    @property
+    def raw(self):
+        """Read RGBC registers
+        return 16 bits red, green, blue and clear data
+        """
+
+        if not self.awake:
+            self.wakeup()
+
+        while not self._valid():
+            time.sleep(0.0024)
+
+        data = tuple(self._read_word(reg) for reg in (_RDATA, _GDATA, _BDATA, _CDATA))
+        return data
+
+    @property
+    def rgb(self):
+        """Read the RGB color detected by the sensor.  Returns a 3-tuple of
+        red, green, blue component values as bytes (0-255).
+        """
+        r, g, b, clear = self.raw
+        if clear:
+            r = int(255 * r / clear)
+            g = int(255 * g / clear)
+            b = int(255 * b / clear)
+        else:
+            r, g, b = 0, 0, 0
+        return r, g, b
+
+    def _valid(self):
+        """Check if RGBC is valid"""
+        return self._read_byte(_STATUS) & 0x01
+
+    def _read_byte(self, address):
+        command = _CMD | address
+        return self.bus.read_byte_data(self.address, command)
+
+    def _read_word(self, address):
+        command = _CMD | _AUTO | address
+        return self.bus.read_word_data(self.address, command)
+
+    def _write_byte(self, address, data):
+        command = _CMD | address
+        self.bus.write_byte_data(self.address, command, data)
+
+    def _write_word(self, address, data):
+        command = _CMD | _AUTO | address
+        data = [(data >> 8) & 0xFF, data & 0xFF]
+        self.bus.write_i2c_block_data(self.address, command, data)
+
+
+Grove = I2cColorSensorV2
+
+
+def main():
+    sensor = I2cColorSensorV2()
+
+    while True:
+        print(sensor.raw)
+        r, g, b = sensor.rgb
+        print('Detected color: #{0:02X}{1:02X}{2:02X}'.format(r, g, b))
+        time.sleep(1.0)
+
+if __name__ == '__main__':
+    main()
