77cf0caa1dfdc88d7d33bda32bcaa9c786bf713a
lanselambor <757016515@qq.com>
2018-07-11 12:27:23 +0000

    add grove rotary angle sensor

---
+++ grove/grove_rotary_angle_sensor.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#
+# This library is for Grove - Rotary Angle Sensor(P)(https://www.seeedstudio.com/Grove-Rotary-Angle-Sensor(P)-p-1242.html)
+#
+# This is the library for Grove Base Hat which used to connect grove sensors for raspberry pi.
+#
+
+'''
+## License
+
+The MIT License (MIT)
+
+Grove Base Hat for the Raspberry Pi, used to connect grove sensors.
+Copyright (C) 2018  Seeed Technology Co.,Ltd. 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+import math
+import sys
+import time
+from grove.adc import ADC
+
+
+class GroveRotaryAngleSensor(ADC):
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+    
+    @property
+    def value(self):
+        return self.adc.read(self.channel)
+
+
+Grove = GroveRotaryAngleSensor
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: {} adc_channel'.format(sys.argv[0]))
+        sys.exit(1)
+
+    sensor = GroveRotaryAngleSensor(int(sys.argv[1]))
+
+    while True:
+        print('Rotary Value: {}'.format(sensor.value))
+        time.sleep(.2)
+
+
+if __name__ == '__main__':
+    main()
