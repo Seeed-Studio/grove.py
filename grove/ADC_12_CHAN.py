80a760875faaedc6ea3ae1fffdd553e5d2c552a5
linux-downey <1148256554@qq.com>
2018-08-30 03:39:19 -0700

    add 12 channels 12 bit adc

---
+++ grove/ADC_12_CHAN.py
@@ -0,0 +1,89 @@
+import time
+from grove.i2c import Bus
+
+ADC_DEFAULT_IIC_ADDR = 0X04
+
+ADC_CHAN_NUM = 8
+
+REG_RAW_DATA_START = 0X10
+REG_VOL_START = 0X20
+REG_RTO_START = 0X30
+
+REG_SET_ADDR = 0XC0
+
+
+class Pi_hat_adc():
+    def __init__(self,bus_num=1,addr=ADC_DEFAULT_IIC_ADDR):
+        self.bus=Bus(bus_num)
+        self.addr=addr
+
+    #The default address is 0x04,but you can change the address to any address
+    def change_i2c_addr(self,addr):
+        self.bus.write_byte_data(self.addr,REG_SET_ADDR,addr)
+        self.addr=addr
+    
+    #get all raw adc data,THe max value is 4095,cause it is 12 Bit ADC
+    def get_all_adc_raw_data(self):
+        array = []
+        for i in range(ADC_CHAN_NUM):  
+            data=self.bus.read_i2c_block_data(self.addr,REG_RAW_DATA_START+i,2)
+            val=data[1]<<8|data[0]
+            array.append(val)
+        return array
+    
+    def get_nchan_adc_raw_data(self,n):
+        data=self.bus.read_i2c_block_data(self.addr,REG_RAW_DATA_START+n,2)
+        val =data[1]<<8|data[0]
+        return val
+    #get all data with unit mv.
+    def get_all_vol_milli_data(self):
+        array = []
+        for i in range(ADC_CHAN_NUM):  
+            data=self.bus.read_i2c_block_data(self.addr,REG_VOL_START+i,2)
+            val=data[1]<<8|data[0]
+            array.append(val)
+        return array
+    
+    def get_nchan_vol_milli_data(self,n):
+        data=self.bus.read_i2c_block_data(self.addr,REG_VOL_START+n,2)
+        val =data[1]<<8|data[0]
+        return val
+
+    #get all data ratio,unit is 0.1%
+    def get_all_ratio_0_1_data(self):
+        array = []
+        for i in range(ADC_CHAN_NUM):  
+            data=self.bus.read_i2c_block_data(self.addr,REG_RTO_START+i,2)
+            val=data[1]<<8|data[0]
+            array.append(val)
+        return array
+    
+    def get_nchan_ratio_0_1_data(self,n):
+        data=self.bus.read_i2c_block_data(self.addr,REG_RTO_START+n,2)
+        val =data[1]<<8|data[0]
+        return val
+
+
+
+ADC = Pi_hat_adc()
+def main():
+    #ADC.change_i2c_addr(0X04)
+    raw_data=ADC.get_all_adc_raw_data()
+    vol_data=ADC.get_all_vol_milli_data()
+    ratio_data=ADC.get_all_ratio_0_1_data()
+    print raw_data
+    print vol_data
+    print ratio_data
+
+    raw=ADC.get_nchan_adc_raw_data(0)
+    print "channel 0 raw data = %d" %raw
+    mili=ADC.get_nchan_vol_milli_data(0)
+    print "channel 0 milli vol = %d mv" %mili
+    ratio=ADC.get_nchan_ratio_0_1_data(0)
+    ratio = ratio*0.1
+    print "channel 0 radio = %f" %ratio+"%"
+    
+    
+
+if __name__ == '__main__':
+    main()
