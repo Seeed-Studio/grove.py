42a5daa0d1ef991f5023bcde2b57aa5ca2362ca3
turmary <turmary@126.com>
2019-04-02 02:42:27 +0000

    Add: document code grove-high-accuracy-temperature

--- grove/grove_high_accuracy_temperature.py
+++ grove/grove_high_accuracy_temperature.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
-# This library is for Grove - I2C High Accuracy Temperature Sensor(MCP9808)
-# (https://www.seeedstudio.com/Grove-I2C-High-Accuracy-Temperature-Sensor-MCP980-p-3108.html)
+# The MIT License (MIT)
+# Copyright (C) 2018  Seeed Technology Co.,Ltd.
 #
-# This is the library for Grove Base Hat which used to connect grove sensors for raspberry pi.
-#
-
+# This is the library for Grove Base Hat
+# which used to connect grove sensors for Raspberry Pi.
 '''
-## License
+This is the code for
+    - `Grove - I2C High Accuracy Temperature Sensor(MCP9808) <https://www.seeedstudio.com/Grove-I2C-High-Accuracy-Temperature-Sensor-MCP980-p-3108.html>`_
 
-The MIT License (MIT)
+Examples:
+    .. code-block:: python
+    import sys
+    import time
+    from grove.factory import Factory
+    from grove.temperature import Temper
 
-Grove Base Hat for the Raspberry Pi, used to connect grove sensors.
-Copyright (C) 2018  Seeed Technology Co.,Ltd. 
+    print("Insert Grove - I2C-High-Accuracy-Temperature")
+    print("  to Grove-Base-Hat any I2C slot")
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+    sensor = Factory.getTemper("MCP9808-I2C")
+    sensor.resolution(Temper.RES_1_16_CELSIUS)
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+    print('Detecting temperature...')
+    while True:
+        print('{} Celsius'.format(sensor.temperature))
+        time.sleep(1)
 '''
 import sys
 import time
 from grove.factory import Factory
 from grove.temperature import Temper
+
+__all__ = ['Temper']
 
 def main():
     print("Insert Grove - I2C-High-Accuracy-Temperature")
