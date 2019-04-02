8205879e6e96be4f9c9159b01c347ce1be1be2e5
turmary <turmary@126.com>
2019-04-02 02:42:27 +0000

    Add: document code grove-high-accuracy-temperature

--- grove/grove_3_axis_compass_bmm150.py
+++ grove/grove_3_axis_compass_bmm150.py
@@ -1,46 +1,22 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# The MIT License (MIT)
+#
+# Grove Base Hat for the Raspberry Pi, used to connect grove sensors.
+# Copyright (C) 2019  Seeed Technology Co.,Ltd.
+#
 # Author: Jon Trulson <jtrulson@ics.com>
 # Copyright (c) 2016-2017 Intel Corporation.
-#
-# The MIT License
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-# 
-# Change log:
-# 
-# 2018-07-13 
-#   Modified by Lambor
-#   To adapt Grove - 3-Axis Digital Compass V2(https://www.seeedstudio.com/s/Grove-3-Axis-Digital-Compass-V2-p-3034.html)
-#   This library is used for Grove Base Hat which used to connect grove sensors for raspberry pi.
-#
-#   Requirement: install python-mraa and python-upm, see the instruction here https://github.com/Seeed-Studio/pi_repo#mraa--upm-package-repository-for-raspberry-pi 
-#
+'''
+This code is for
+    - `Grove - 3-Axis Digital Compass V2 <https://www.seeedstudio.com/s/Grove-3-Axis-Digital-Compass-V2-p-3034.html>`_
+
+'''
 
 from __future__ import print_function
 import time, sys, signal, atexit, math
-try:
-    from upm import pyupm_bmm150 as sensorObj
-except ImportError:
-    print('Error: Please install python-mraa python-upm module.\r\n' 
-          'See instruction here https://github.com/Seeed-Studio/pi_repo#mraa--upm-package-repository-for-raspberry-pi ')
+from upm import pyupm_bmm150 as sensorObj
 
 
 def main():
