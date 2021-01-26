7f6a9a7667bf29b60f4b6b0803853029809266b2
Mark Winter <wintermarkedward@gmail.com>
2021-01-26 14:23:42 +0900

    Add AHT20 Sensor

---
+++ grove/grove_temperature_humidity_aht20.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#
+# Library for Grove - I2C Industrial Grade Temperature & Humidity Sensor (AHT20)
+# (https://wiki.seeedstudio.com/Grove-AHT20-I2C-Industrial-Grade-Temperature&Humidity-Sensor/)
+#
+
+'''
+## License
+
+The MIT License (MIT)
+
+Copyright (C) 2018  Seeed Technology Co.,Ltd. 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+import time
+from grove.i2c import Bus
+
+
+class GroveTemperatureHumidityAHT20(object):
+    def __init__(self, address=0x38, bus=None):
+        self.address = address
+
+        # I2C bus
+        self.bus = Bus(bus)
+
+    def read(self):
+        self.bus.write_i2c_block_data(self.address, 0x00, [0xac, 0x33, 0x00])
+
+        # measurement duration < 16 ms
+        time.sleep(0.016)
+
+        data = self.bus.read_i2c_block_data(self.address, 0x00, 6)
+
+        humidity = data[1]
+        humidity <<= 8
+        humidity += data[2]
+        humidity <<= 4
+        humidity += (data[3] >> 4)
+        humidity /= 1048576.0
+        humidity *= 100
+
+        temperature = data[3] & 0x0f
+        temperature <<= 8
+        temperature += data[4]
+        temperature <<= 8
+        temperature += data[5]
+        temperature = temperature / 1048576.0*200.0-50.0  # Convert to Celsius
+
+        return temperature, humidity
+
+
+def main():
+    sensor = GroveTemperatureHumidityAHT20()
+    while True:
+        temperature, humidity  = sensor.read()
+
+        print('Temperature in Celsius is {:.2f} C'.format(temperature))
+        print('Relative Humidity is {:.2f} %'.format(humidity))
+
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
